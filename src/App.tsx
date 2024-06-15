3c7ec202e88983bbef72fba2d4275a187b839554
Ivan-ISS <vanya5@inbox.ru>
2024-06-15 14:09:01 +0500

    project_shop: styles reset is configured, fonts are connected

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,11 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css'; */
 
-function App() {
+export default function App() {
 
-
-  return (
-    <>
-     
-    </>
-  );
+    return (
+        <>
+            Begin
+        </>
+    );
 }
-
-export default App;
