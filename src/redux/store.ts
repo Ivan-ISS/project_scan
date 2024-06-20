80fb7dda24a7d502ef505abee2ec6648899e5562
Ivan-ISS <vanya5@inbox.ru>
2024-06-20 20:34:00 +0500

    project_scan: persistent storage connected

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,31 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference types="redux-persist" />
+import storage from 'redux-persist/lib/storage';
+import { persistReducer } from 'redux-persist';
+import { persistStore } from 'redux-persist';
+import { FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
 import { configureStore } from '@reduxjs/toolkit';
 import { useSelector, useDispatch, TypedUseSelectorHook } from 'react-redux';
 import { authSlice } from './slices/authSlice/authSlice';
 import { userSlice } from './slices/userSlice/userSlice';
 
+const persistConfig = {
+    key: 'root',
+    storage
+};
+
+const persistedReducer = persistReducer(persistConfig, authSlice.reducer);
+
 const store = configureStore({
     reducer: {
-        auth: authSlice.reducer,
+        auth: persistedReducer,
         user: userSlice.reducer,
     },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+        serializableCheck: {
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+        },
+    })
 });
 
 export { store };
@@ -15,3 +33,4 @@ export type RootState = ReturnType<typeof store.getState>;
 export type RootDispatch = typeof store.dispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 export const useAppDispatch = () => useDispatch<RootDispatch>();
+export const persistor = persistStore(store);
